145511f73162bfecd55380ecb2aaca850326d882
Sergei Poznanski <cepr0@ukr.net>
2019-08-28 19:21:49 +0300

    Clean up the code

--- src/main/java/io/github/cepr0/demo/api/CarController.java
+++ src/main/java/io/github/cepr0/demo/api/CarController.java
@@ -1,27 +1,11 @@
-/*
- * Copyright 2019 Generic-CRUD contributors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.github.cepr0.demo.api;
 
 import io.github.cepr0.crud.api.AbstractCrudController;
 import io.github.cepr0.crud.api.OnCreate;
 import io.github.cepr0.crud.api.OnUpdate;
-import io.github.cepr0.demo.model.Car;
 import io.github.cepr0.demo.dto.CarRequest;
 import io.github.cepr0.demo.dto.CarResponse;
+import io.github.cepr0.demo.model.Car;
 import io.github.cepr0.demo.service.CarService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
